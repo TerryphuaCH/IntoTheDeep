a9fc6b8c366a25ae971b1aa8ad0e0f74c652d681
LOW ZI HONG <92472965+LOWZIHONG@users.noreply.github.com>
2022-12-14 23:59:34 +0800

    Update UIManager.cs

--- Assets/DDA/Script/UIManager.cs
+++ Assets/DDA/Script/UIManager.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UIManager instance;
+
+    public GameObject LoginUI;
+    public GameObject RegisterUI;
+
+
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+
+    }
+    //Functions to change the login screen UI
+    public void LoginScreen() //Back button
+    {
+        LoginUI.SetActive(true);
+        RegisterUI.SetActive(false);
+
+    }
+    public void RegisterScreen() // Register button
+    {
+        LoginUI.SetActive(false);
+        RegisterUI.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
